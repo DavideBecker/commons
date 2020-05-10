09b91452f2bd30a5a09cd44df45e46c9d230a6b5
Davide Becker <github@davide.me>
2020-05-10 18:57:09 +0200

    Improve jumpstart-server script

--- scripts/jumpstart-server.sh
+++ scripts/jumpstart-server.sh
@@ -44,7 +44,8 @@ sudo apt update
 
 ################################################################################
 # Add new user
-    printf "How should the new user be called? >"
+    echo ""
+    echo -n "How should the new user be called? > "
     read USERNAME
 
     # Add new user
@@ -52,6 +53,9 @@ sudo apt update
     adduser $USERNAME
     # Make him sudo
     usermod -aG sudo $USERNAME
+    echo ""
+    echo "Added user $USERNAME"
+    echo ""
 
     #####
     # Disable passwords & restart ssh
@@ -65,12 +69,16 @@ sudo apt update
         sshConfig_disable $REGEX__PermitRootLogin
 
         systemctl restart sshd
+        echo ""
+        echo "Disabled password and root login. Make sure you enter a SSH key in the next step."
+        echo "Otherwise you will lock yourself out of the server after disconnecting."
+        echo ""
     #####
 
 
 ################################################################################
-# Add add file for user context
-cat << 'EOF' >> /home/$USERNAME/jumpstart.sh
+# Add add file for executing the rest in user context
+cat >/home/$USERNAME/jumpstart.sh << 'EOT'
 #!/bin/bash
 
 # Terminal colors
@@ -83,29 +91,38 @@ cyan=$(tput setaf 6 || tput AF 6)
 reset=$(tput sgr0)
 
 function add_ssh_key {
+    echo ""
     echo -n "Paste the SSH key you want to add to the user $USERNAME. >"
     read SSH_KEY
     echo $SSH_KEY >> ~/.ssh/authorized_keys
+    echo ""
+
+    echo "Added the key to the authorized_keys file"
+    echo ""
 }
 
 function generate_ssl_cert {
-    echo -n "Which domain do you want to add a certificate for? (google.de)"
-    echo -n "Enter n to cancel"
+    echo ""
+    echo "Which domain do you want to add a certificate for? (google.de)"
+    echo -n "Enter n to cancel "
     read SSL_DOMAIN
     if [$SSL_DOMAIN != 'n']; then
         sudo certbot --apache -d $SSL_DOMAIN
     fi
+    echo ""
 }
 
 function ask_user {
+    echo ""
     response="x"
-    printf "$1 (y | n)"
+    echo -n "$1 (y | n)"
     while [ "$response" != "y" ] && [ "$response" != "n" ]; do
-        echo -n "> "
+        echo "> "
         read response
     done
     local  __result=$2
     eval $__result=$response
+    echo ""
 }
 
 REGEX__ChallengeResponseAuthentication="ChallengeResponseAuthentication[[:space:]]+"
@@ -124,6 +141,9 @@ URL__NODEJS_SETUP="https://deb.nodesource.com/setup_12.x"
     touch ~/.ssh/authorized_keys
     chmod 700 ~/.ssh
     chmod 600 ~/.ssh/authorized_keys
+    echo ""
+    echo "Creating SSH folders and setting restrictive permissions"
+    echo ""
     add_ssh_key
 
 
@@ -139,21 +159,24 @@ URL__NODEJS_SETUP="https://deb.nodesource.com/setup_12.x"
 
     #####
     # Optionally mount relevant apache folders to user directory
-        # printf "Do you want to mount apache folders to your home directory? (Recommended)"
-        # ask_user "This will allow you to edit apache configs without root permissions from within your user directory" MOUNT_APACHE_CONFIGS
-        # if [ $MOUNT_APACHE_CONFIGS == "y" ]; then
-        #     # Mount apache folders
-        #     apt install -y bindfs
-        #     mkdir apache
-        #     sudo bindfs#$FOLDER__APACHE_SITES_AVAILABLE /home/$USER/apache/sites fuse force-user=$USER,force-group=$USER,create-for-user=$USER__APACHE,create-for-group=$USER__APACHE,create-with-perms=0770,chgrp-ignore,chown-ignore,chmod-ignore 0 0
-        # fi
+        echo "Do you want to mount apache folders to your home directory? (Recommended)"
+        ask_user "This will allow you to edit apache configs without root permissions from within your user directory" MOUNT_APACHE_CONFIGS
+        if [ $MOUNT_APACHE_CONFIGS == "y" ]; then
+            # Mount apache folders
+            apt install -y bindfs
+            mkdir -p /home/$USER/apache/sites
+            chown -Rf $USER:$USER /home/$USER/apache/sites
+            chmod -Rf 770 /home/$USER/apache/sites
+            echo "bindfs#$FOLDER__APACHE_SITES_AVAILABLE /home/$USER/apache/sites fuse force-user=$USER,force-group=$USER,create-for-user=$USER__APACHE,create-for-group=$USER__APACHE,create-with-perms=0770,chgrp-ignore,chown-ignore,chmod-ignore 0 0" | sudo tee /etc/fstab
+            sudo mount /home/$USER/apache/sites
+        fi
     #####
 
     #####
     # Optionally set up let's encrypt
         ask_user "Do you want to setup Let's Encrypt?" SETUP_LETS_ENCRYPT
         if [ $SETUP_LETS_ENCRYPT == "y" ]; then
-            # Mount apache folders
+            # Setup Let's Encrypt
             sudo add-apt-repository ppa:certbot/certbot
             sudo apt install -y python-certbot-apache
         fi
@@ -189,13 +212,27 @@ URL__NODEJS_SETUP="https://deb.nodesource.com/setup_12.x"
     sudo ufw allow 'OpenSSH'
     sudo ufw enable
     sudo ufw app list
-EOF
+EOT
+
 
 ################################################################################
-# Switch to user
-su - $USERNAME
+# Change ownership of the user jumpstart script
+chown $USERNAME:$USERNAME /home/$USERNAME/jumpstart.sh
+
 
 ################################################################################
-# Remove this file
+# Continue by executing the rest of the script as the new user
+# This ensures some permissions and files are in a user context, not root
+sudo -u $USERNAME /home/$USERNAME/jumpstart.sh
+
+
+################################################################################
+# Remove this file and the generated user script
 # Goodbye cruel world
 rm -- "$0"
+rm /home/$USERNAME/jumpstart.sh
+
+
+################################################################################
+# Switch to new user
+su - $USERNAME
